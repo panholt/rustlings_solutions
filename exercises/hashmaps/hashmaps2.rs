c25f7bd99a93498d4fb3f984da9d2bfd9d983af8
Paul Anholt <panholt@gmail.com>
2023-01-13 19:02:59 -0600

    Inital commit

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -11,7 +11,7 @@
 //
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -37,6 +37,11 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+        match fruit {
+            Fruit::Banana => basket.insert(fruit, 1),
+            Fruit::Pineapple => basket.insert(fruit, 10),
+            _ => Option::None,
+        };
     }
 }
 
--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,7 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -40,6 +40,18 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        for result in [(team_1_name, team_1_score, team_2_score), (team_2_name, team_2_score, team_1_score)]{
+            let entry = scores.entry(result.0.clone()).or_insert(
+                Team{
+                    name: result.0,
+                    goals_scored: 0,
+                    goals_conceded: 0
+                }
+            );
+            entry.goals_scored += result.1;
+            entry.goals_conceded += result.2;
+        }
+
     }
     scores
 }
